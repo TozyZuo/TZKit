c17b907f65e9077a8a62cc842ec066ee3d7ee68e
TozyZuo <haoyuan.lhy@alibaba-inc.com>
2019-01-21 17:48:09 +0800

    fix error

--- macOS/Category/AppKitExtension.m
+++ macOS/Category/AppKitExtension.m
@@ -8,35 +8,7 @@
 
 #import <Cocoa/Cocoa.h>
 #import <objc/runtime.h>
-
-@implementation NSObject (AppKitExtension)
-
-BOOL ClassImplementedSelector(Class aClass, SEL aSelector)
-{
-    if ([aClass instancesRespondToSelector:aSelector]) {
-        IMP imp = method_getImplementation(class_getInstanceMethod(aClass, aSelector));
-        IMP superIMP = method_getImplementation(class_getInstanceMethod(class_getSuperclass(aClass), aSelector));
-        return (!superIMP && imp) || (imp != superIMP);
-    }
-    return NO;
-}
-
-+ (BOOL)implementedSelector:(SEL)aSelector
-{
-    return ClassImplementedSelector(object_getClass(self), aSelector);
-}
-
-+ (BOOL)instancesImplementedSelector:(SEL)aSelector
-{
-    return ClassImplementedSelector(self, aSelector);
-}
-
-- (BOOL)implementedSelector:(SEL)aSelector
-{
-    return ClassImplementedSelector(self.class, aSelector);
-}
-
-@end
+#import "NSObject+TZCategory.h"
 
 @implementation NSView (AppKitExtension)
 
